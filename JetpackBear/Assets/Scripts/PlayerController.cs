6193a1b7b155a0426d2d70f6af637629d9c17051
LeandroDotta <leandro.dotta@yahoo.com.br>
2017-08-12 00:56:36 -0300

    Setup

---
+++ JetpackBear/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float forceX;
+	
+	
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+	}
+}

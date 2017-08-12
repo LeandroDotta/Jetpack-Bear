08a61fb3d5519d88bfac39a7b3e6d88349710082
LeandroDotta <leandro.dotta@yahoo.com.br>
2017-08-12 03:36:37 -0300

    Movimentação básica do personagem

--- JetpackBear/Assets/Scripts/PlayerController.cs
+++ JetpackBear/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float forceX;
-	
-	
+	public float flyForce;
+	public float moveForce;
 
+	private bool holdFly;
+	private float axisHorizontal;
+
+	private Rigidbody2D rb2d;
+
+	private Animator burstAnim1;
+	private Animator burstAnim2;
 
 	void Start () {
-		
+		rb2d = GetComponent<Rigidbody2D>();
+		burstAnim1 = transform.Find("JetpackBurst1").GetComponent<Animator>();
+		burstAnim2 = transform.Find("JetpackBurst2").GetComponent<Animator>();
 	}
-	
-	void Update () {
 
+	void FixedUpdate()
+	{
+		if(holdFly)
+			rb2d.AddForce(Vector2.up * flyForce);
+
+		rb2d.AddForce(new Vector2(axisHorizontal * moveForce, 0));	
+	}
+
+	void Update () {
+		holdFly = Input.GetButton("Fly") || Input.GetAxisRaw("Vertical") == 1;
+		axisHorizontal = Input.GetAxis("Horizontal");
+
+		burstAnim1.SetBool("on", holdFly);
+		burstAnim2.SetBool("on", holdFly);
+
+		Vector3 scale = transform.localScale;
+		if(rb2d.velocity.normalized.x > 0)
+			scale.x = 1;
+		else if(rb2d.velocity.normalized.x < 0)
+			scale.x = -1;
+
+		transform.localScale = scale;
+
+		transform.rotation = Quaternion.Euler(0, 0, -(rb2d.velocity.x*3));
 	}
 }
---
+++ JetpackBear/Assets/Scripts/UITests.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITests : MonoBehaviour {
+
+	[Header("Velocity")]
+	public Text velocityText;
+	public Rigidbody2D theRigidbody;
+
+	void Update () {
+		velocityText.text = "Velocity: " + theRigidbody.velocity;
+	}
+}

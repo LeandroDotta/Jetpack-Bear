5503838a09511b7a3e99ae6c1e91bf37d46f1a23
LeandroDotta <leandro.dotta@yahoo.com.br>
2017-09-11 02:46:42 -0300

    Reshaping the levels
    - Reshaped stage 1
    - Removed the compression of all sprites
    - Created an editor wizard that creates random trees to use in the
    background

---
+++ JetpackBear/Assets/Editor/SpriteProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+public class SpriteProcessor : AssetPostprocessor {
+
+	void OnPostprocessTexture(Texture2D texture)
+    {
+        string lowerCaseAssetPath = assetPath.ToLower ();
+        bool isInSpritesDirectory = lowerCaseAssetPath.IndexOf ("/sprites/") != -1;
+
+        if (isInSpritesDirectory) 
+        {
+            TextureImporter textureImporter = (TextureImporter) assetImporter;
+            textureImporter.textureType = TextureImporterType.Sprite;
+			textureImporter.spritePixelsPerUnit = 32;
+			textureImporter.filterMode = FilterMode.Point;
+			textureImporter.textureCompression = TextureImporterCompression.Uncompressed;
+        }
+    }
+}
